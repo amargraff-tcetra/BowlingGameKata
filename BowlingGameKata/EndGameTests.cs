a1936f87773700c32f180894a161df3ddaad3ead
Aaron Margraff <amargraff@tcetra.com>
2024-01-30 17:10:33 -0500

    wip failing 10th frame spare

--- BowlingGameKata/EndGameTests.cs
+++ BowlingGameKata/EndGameTests.cs
@@ -109,7 +109,86 @@ namespace BowlingGameKata
             _ = game.Score();
             Assert.Equal(300, game.Score());
             Assert.Equal(10, game.Frames.Count);
-        }
+        } 
+        
+        [Fact]
+        public void TenthFrameSpareTest()
+        {
+            var game = new Game();
+            game.Roll(0);//Frame 1
+            game.Roll(0);
 
+            game.Roll(0);//Frame 2
+            game.Roll(0);
+
+            game.Roll(0);//Frame 3
+            game.Roll(0);
+
+            game.Roll(0);//Frame 4
+            game.Roll(0);
+
+            game.Roll(0);//Frame 5
+            game.Roll(0);
+
+            game.Roll(0);//Frame 6
+            game.Roll(0);
+
+            game.Roll(0);//Frame 7
+            game.Roll(0);
+
+            game.Roll(0);//Frame 8
+            game.Roll(0);
+
+            game.Roll(0);//Frame 9
+            game.Roll(0);
+
+            game.Roll(3);//Frame 10
+            game.Roll(7);
+            game.Roll(5);
+
+            _ = game.Score();
+            Assert.Equal(20, game.Score());
+            Assert.Equal(10, game.Frames.Count);
+        } 
+        
+        [Fact]
+        public void NormalGameTest()
+        {
+            var game = new Game();
+            game.Roll(0);//Frame 1 --9
+            game.Roll(9);
+
+            game.Roll(0);//Frame 2 --19/
+            game.Roll(10);
+
+            game.Roll(5);//Frame 3 --32
+            game.Roll(3);
+
+            game.Roll(4);//Frame 4 --42/
+            game.Roll(6);
+
+            game.Roll(10);//Frame 5 --62X
+
+            game.Roll(1);//Frame 6 --74
+            game.Roll(5);
+
+            game.Roll(5);//Frame 7 --83
+            game.Roll(4);
+
+            game.Roll(3);//Frame 8 --91
+            game.Roll(5);
+
+            game.Roll(10);//Frame 9 --101X
+
+            game.Roll(8);//Frame 10 --119
+            game.Roll(1);
+
+            //Shouldn't count this roll.
+            game.Roll(5);
+
+            _ = game.Score();
+            Assert.Equal(119, game.Score());
+            Assert.Equal(10, game.Frames.Count);
+        } 
     }
 }
--- BowlingGameKata/Models/Game.cs
+++ BowlingGameKata/Models/Game.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Formats.Asn1.AsnWriter;
@@ -27,6 +28,7 @@ namespace BowlingGameKata.Models
             {
                 Rolls.Add(pins);
             }
+            //Running Score
             _ = Score();
         }
 
@@ -41,21 +43,18 @@ namespace BowlingGameKata.Models
             {
                 currentFrame.Rolls.Add(roll.value);
 
-                //Frame complete
-                if (currentFrame.Rolls.Sum() == PIN_COUNT || currentFrame.Rolls.Count == ALLOWED_ROLLS || Frames.Count + 1 == ALLOWED_FRAMES)
-                {
-                    var tenthFrameBonusRoll = Frames.Count + 1 == ALLOWED_FRAMES;
-                    tenthFrameBonusRoll &= currentFrame.Rolls.Take(2).Sum() <= PIN_COUNT;
-                    tenthFrameBonusRoll &= currentFrame.Rolls.Count < ALLOWED_TENTH_FRAME_ROLLS;
+                var spare = currentFrame.Rolls.Count == ALLOWED_ROLLS && currentFrame.Rolls.Take(2).Sum() == PIN_COUNT;
+                var strike = currentFrame.Rolls.Count == 1 && currentFrame.Rolls.Sum() == PIN_COUNT;
+                var tenthFrameBonusRoll = Frames.Count + 1 == ALLOWED_FRAMES && (spare || strike);
 
-                    //Spare
-                    if ((currentFrame.Rolls.Sum() == PIN_COUNT && currentFrame.Rolls.Count == ALLOWED_ROLLS) || tenthFrameBonusRoll)
+                //Frame complete
+                if (currentFrame.Rolls.Sum() == PIN_COUNT || currentFrame.Rolls.Count == ALLOWED_ROLLS || tenthFrameBonusRoll)
+                {
+                    if (spare)
                     {
                         currentFrame.Bonus = Rolls.ElementAtOrDefault(roll.index + 1);//Next roll counts as bonus
                     }
-
-                    //Strike
-                    if ((currentFrame.Rolls.Sum() == PIN_COUNT && currentFrame.Rolls.Count < ALLOWED_ROLLS) || tenthFrameBonusRoll)
+                    else if (strike)
                     {
                         currentFrame.Bonus = Rolls.ElementAtOrDefault(roll.index + 1) + Rolls.ElementAtOrDefault(roll.index + 2);//Next two rolls count as bonus
                     }
